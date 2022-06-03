e03f5a2406d425043a8674f2b09ea98986afcfae
Jorge Sandoval <82074301+jorsandoval@users.noreply.github.com>
2022-06-02 20:29:36 -0400

    avance cochino, pero avance al fin

--- js/script.js
+++ js/script.js
@@ -6,3 +6,11 @@ function getBasePath(service) {
 function createRowDisaster(reference, magnitude, latitude, longitude, local_time) {
     return '<tr><td>' + reference + '</td><td>' + magnitude + '</td><td>' + latitude + '</td><td>' + longitude + '</td><td>' + local_time + '</td></tr>';
 }
+
+function createRowOnemiDisaster(state, post_title, alert_region, alert_date, url) {
+    return '<tr><td>' + state + '</td><td>' + post_title + '</td><td>' + alert_region + '</td><td>' + alert_date + '</td><td><a href="' + url + '">' + url + '</a></td></tr>';
+}
+
+function createRowMundoDisaster(reference, magnitude, latitude, longitude, utc_time) {
+    return '<tr><td>' + reference + '</td><td>' + magnitude + '</td><td>' + latitude + '</td><td>' + longitude + '</td><td>' + utc_time + '</td></tr>';
+}
